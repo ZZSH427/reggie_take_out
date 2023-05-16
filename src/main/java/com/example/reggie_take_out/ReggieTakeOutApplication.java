355050ac39941909399871277e033f9a7b310a74
ZZSH427 <c2012727@newcastle.ac.uk>
2023-05-16 20:48:00 +0100

    Initial commit

---
+++ src/main/java/com/example/reggie_take_out/ReggieTakeOutApplication.java
@@ -0,0 +1,19 @@
+package com.example.reggie_take_out;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@Slf4j
+@ServletComponentScan
+public class ReggieTakeOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReggieTakeOutApplication.class, args);
+		log.info("项目启动成功");
+	}
+
+}
---
+++ src/main/java/com/example/reggie_take_out/controller/EmployeeController.java
@@ -0,0 +1,82 @@
+package com.example.reggie_take_out.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.reggie_take_out.common.R;
+import com.example.reggie_take_out.entity.Employee;
+import com.example.reggie_take_out.service.EmployeeService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        String password = employee.getPassword();
+        password=DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        if (emp==null){
+            return R.error("登陆失败");
+
+        }
+        if (!emp.getPassword().equals(password)) {
+            return R.error("登陆失败");
+        }
+        if (emp.getStatus()==0){
+            return R.error("账号已禁用");
+        }
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+
+
+    }
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
+    @PostMapping
+    public R<String>save(HttpServletRequest request, @RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()) );
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empID =(Long) request.getSession().getAttribute("employee") ;
+        employee.setCreateUser(empID);
+        employee.setUpdateUser(empID);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+    @GetMapping("/page")
+    public  R<Page> page(int page,int pageSize,String name){
+        log.info("page ={}, pageSize={},name ={}",page,pageSize,name);
+        Page pageInfo =new Page(page,pageSize);
+        LambdaQueryWrapper<Employee> queryWrapper =new LambdaQueryWrapper();
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+}
