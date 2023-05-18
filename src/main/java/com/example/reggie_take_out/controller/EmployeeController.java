8a0f133976dc37051c0f662e6b06123cefb6e6ab
ZZSH427 <c2012727@newcastle.ac.uk>
2023-05-18 23:44:27 +0100

    1

--- src/main/java/com/example/reggie_take_out/controller/EmployeeController.java
+++ src/main/java/com/example/reggie_take_out/controller/EmployeeController.java
@@ -79,4 +79,14 @@ public class EmployeeController {
 
         return R.success(pageInfo);
     }
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee){
+        log.info(employee.toString());
+
+        Long empId=(Long)request.getSession().getAttribute("employee") ;
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
 }
